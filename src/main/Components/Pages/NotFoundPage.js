a123c0c30ac75c1ab116b25ef52c1ad42845d8eb
Gerson Arias <gerarca88@gmail.com>
2021-12-09 09:15:29 -0400

    fixing router

--- src/main/Components/Pages/NotFoundPage.js
+++ src/main/Components/Pages/NotFoundPage.js
@@ -1,29 +1,59 @@
 import React from 'react';
-import {
-    Box,
-    Grid
-} from "@material-ui/core";
 import { Link, Redirect } from 'react-router-dom';
-import ImgStruard from "../../assets/img/stuard.png";
+import { Typography, useMediaQuery, Button, Grid, Box } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from "@material-ui/core/Paper";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import SchoolIcon from "@material-ui/icons/School";
+import ResponsiveImgMaterialUi from "responsive-img-material-ui";
+import main from "../../assets/img/brain.gif";
 
-class NotFoundPage extends React.Component{
-    render(){
-        return <div> 
-                    <Box px={4} m={{ xs:15 }} textAlign="center" justifyContent="center">
-                        <Grid container xs={12} spacing={2} alignItems="center" justifyContent="center">
-                            <Box  col={12}> 
-                                <div style={{ margin:'auto' }}>
-                                    <img src={ImgStruard} alt="Stuard" width="300"/>
-                                </div>                         
-                                <h1 className="text404"> Error 404 Page Not Found </h1>                        
-                                <p style={{textAlign:"center"}}>
-                                <Link to="/">Go to Home </Link>
-                                </p>
-                                <Redirect to="/" />
-                            </Box>                            
-                        </Grid>
-                    </Box>
-                </div>;
+const useStyles = makeStyles((theme) => ({
+    textMSG: {
+        ...theme.typography.textMSG,
     }
-}
+  }));
+
+const NotFoundPage = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+    return (
+        <Paper style={{ background: 'black', height: '100vh'}}>
+            <box md={{ flexGrow: 1 }}>
+                <Grid container spacing={2} alignItems="center" justifyContent="center">
+                    <Grid item md={6}>
+                        <Box col="12" pt={{ xs:10,  md:15 }} textAlign="center" >
+                            <Typography variant="h1">Engineer Gerson Arias</Typography>
+                            <Typography variant="h4">Software Developer</Typography>                       
+                            <ResponsiveImgMaterialUi xs={main} />
+                        </Box>        
+                    </Grid>
+                    <Grid item md={6} >
+                        <Box col="12" pt={{ md:25 }}  >
+                            <p className={classes.textMSG} >"Rise and Rise Until Lambs Become Lions"</p>
+                        </Box>             
+                    </Grid>
+                    {isMobile ? (
+                    <Box textAlign="right" mt={20}>
+                        <Button                    
+                        component={Link}
+                        variant="contained"
+                        color="primary"
+                        to="/Personal"
+                        >
+                        <SchoolIcon style={{ marginRight: 15 }} />
+                        <Typography variant="button">Personal</Typography>
+                        <ChevronRightIcon />
+                        </Button>
+                    </Box>
+                    ) : (
+                    <> </>
+                    )} 
+                </Grid>
+            </box>
+            <Redirect to="/" />
+        </Paper>
+    );
+};
+
 export default NotFoundPage;
